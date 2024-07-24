c96f7776b9023445e24ea59cf2f18a598110ff2b
Gabriel Barbosa de Oliveira <gabrieloliveira00@outlook.com>
2024-07-24 14:09:12 -0300

    chart changes

--- src/stocks/static/dashboard.js
+++ src/stocks/static/dashboard.js
@@ -7,18 +7,17 @@ const slug = watchStock.slug;
 let webSocketUrl;
 let socket;
 
-let chartData = [];
-
 const ctx = document.getElementById("stock-price-chart");
 let chart;
 
-// functions
-const formatDate = (date) => {
-	const minutes = String(date.getMinutes()).padStart(2, "0");
-	const hours = String(date.getHours()).padStart(2, "0");
+let prices = [];
+let times = [];
 
-	let ret = `${hours}:${minutes}`;
-	return ret;
+// test variables
+const tunel = {
+	basePrice: 38,
+	range: 0.01,
+	interval: 5, // minutes
 };
 
 const fetchPrices = async () => {
@@ -27,38 +26,49 @@ const fetchPrices = async () => {
 	}
 	const response = await fetch(`${window.location.href}prices/`);
 	const data = await response.json();
-	chartData = data.prices.map((el) => ({
-		label: formatDate(new Date(el.timestamp)),
-		data: el.price,
-	}));
+	prices = data.prices.map((el) => Number(el.price));
+	times = data.prices.map((el) => new Date(el.timestamp));
 };
 
 const drawChart = async () => {
-	if (chartData.length < 1) {
+	if (prices.length < 1) {
 		await fetchPrices();
 	}
+
+	lastTime = times[times.length - 1];
+	time_history = 30;
+	time_forecast = 15;
+	maxTime = new Date(lastTime.getTime() + time_forecast * 60000);
+	minTime = new Date(lastTime.getTime() - time_history * 60000);
 
 	chart = new Chart(ctx, {
 		type: "line",
 		data: {
-			labels: chartData.map((el) => el.label),
+			labels: times,
 			datasets: [
 				{
 					label: "price",
-					data: chartData.map((el) => el.data),
+					data: prices,
 					borderWidth: 1,
 				},
 			],
 		},
 		options: {
+			responsive: true,
 			scales: {
-				y: {
-					beginAtZero: true,
-				},
 				x: {
-					ticks: {
-						maxTicksLimit: 30,
+					type: "time",
+					time: {
+						displayFormats: {
+							minute: "HH:mm",
+						},
 					},
+					min: minTime,
+					max: maxTime,
+				},
+				y: {
+					min: tunel.basePrice - tunel.basePrice * tunel.range,
+					max: tunel.basePrice + tunel.basePrice * tunel.range,
 				},
 			},
 		},
@@ -66,24 +76,32 @@ const drawChart = async () => {
 };
 
 const updateChart = () => {
-	chart.data.datasets[0].data.push(chartData[chartData.length - 1].data);
-	chart.data.labels.push(chartData[chartData.length - 1].label);
+	chart.data.datasets[0].data.push(prices[prices.length - 1]);
+	chart.data.labels.push(times[times.length - 1]);
+
+	lastTime = times[times.length - 1];
+	time_history = 30;
+	time_forecast = 15;
+	maxTime = new Date(lastTime.getTime() + time_forecast * 60000);
+	minTime = new Date(lastTime.getTime() - time_history * 60000);
+
+	chart.options.scales.x.min = minTime;
+	chart.options.scales.x.max = maxTime;
+
 	chart.update();
 };
 
 // main
+drawChart();
+
 if (slug) {
 	webSocketUrl = `ws://${window.location.host}/ws/stocks/${slug}/`;
 	socket = new WebSocket(webSocketUrl);
 
 	socket.onmessage = function (e) {
 		data = JSON.parse(e.data);
-		chartData = data.prices.map((el) => ({
-			label: formatDate(new Date(el.timestamp)),
-			data: el.price,
-		}));
+		prices = data.prices.map((el) => Number(el.price));
+		times = data.prices.map((el) => new Date(el.timestamp));
 		updateChart();
 	};
 }
-
-drawChart();
